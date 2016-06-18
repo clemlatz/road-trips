84a62102f30824746886490d57e356e1705c350a
Clement Bourgoin <cb@nokto.net>
2016-06-18 18:04:53 +0200

    Get entries from server and add pins to the map

--- app.js
+++ app.js
@@ -3,9 +3,74 @@
 
 'use strict';
 
+// MAP
+
+class Map {
+
+  constructor(element) {
+    this.element = element;
+
+    this.buildMap();
+    this.getEntries();
+  }
+
+  /**
+   * Insert Google map
+   */
+  buildMap() {
+    this.map = new google.maps.Map(this.element, {
+      center: { lat: 64.9313, lng: -19.0212 },
+      zoom: 7
+    });
+  }
+
+  /**
+   * Fetch entries from server and create Pins
+   */
+  getEntries() {
+    const map = this.map;
+    fetch('/data/entries.json')
+      .then(function(response) {
+        return response.json();
+      }).then(function(entries) {
+        entries.forEach(function(entry) {
+          new Pin(entry, map);
+        });
+      });
+  }
+}
+
+// PIN
+
+class Pin {
+
+  constructor(entry, map) {
+    this.entry = entry;
+    this.map = map;
+
+    this.addMarker();
+  }
+
+  /**
+   * Add marker to the map
+   */
+  addMarker() {
+    new google.maps.Marker({
+      position: { lat: this.entry.coords[0], lng: this.entry.coords[1] },
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      label: this.entry.id
+    });
+  }
+
+}
+
+/**
+ * Called by Google Maps API JS when loaded
+ */
 function initMap() {
-  new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 64.9313, lng: -19.0212 },
-    zoom: 7
-  });
+  const map = document.querySelector('#map');
+  if (map) {
+    new Map(map);
+  }
 }
