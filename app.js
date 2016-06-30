f1232b221408026c1bee5018cb3f1ebf17f37117
Clement Bourgoin <cb@nokto.net>
2016-06-30 14:08:41 +0200

    Display clicked photos in a lightbox

--- app.js
+++ app.js
@@ -153,6 +153,10 @@ class Photo {
     image.alt = this.caption;
     this.element.appendChild(image);
 
+    image.addEventListener('click', function() {
+      window.lightbox.show(this);
+    }.bind(this));
+
     const text = document.createElement('p');
     text.classList.add('caption');
     text.textContent = this.caption;
@@ -160,13 +164,83 @@ class Photo {
 
     return this.element;
   }
+}
 
+// LIGHTBOX
+
+class Lightbox {
+
+  constructor(element) {
+    this.element = element;
+    this.overlay = element.parentNode;
+    this.imageRatio = 1.28;
+
+    this.overlay.addEventListener('click', this.hide.bind(this));
+  }
+
+  getDimensions() {
+    const w = window, d = document, e = d.documentElement, g = d.getElementsByTagName('body')[0],
+      windowWidth = w.innerWidth || e.clientWidth || g.clientWidth,
+      windowHeight = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+    let targetHeight = windowHeight * 0.95,
+      targetWidth = targetHeight * this.imageRatio,
+      targetTop = windowHeight * 0.025,
+      targetLeft = windowWidth / 2 - targetWidth / 2;
+
+    if (targetWidth > windowWidth * 0.95) {
+      targetWidth = windowWidth * 0.95;
+      targetHeight = targetWidth / this.imageRatio;
+      targetLeft = windowWidth * 0.025;
+      targetTop = windowHeight / 2 - targetHeight / 2;
+    }
+
+    return {
+      top: targetTop,
+      left: targetLeft,
+      width: targetWidth,
+      height: targetHeight
+    };
+  }
+
+  resize() {
+    const dimensions = this.getDimensions();
+
+    this.element.style.width = dimensions.width + 'px';
+    this.element.style.height = dimensions.height + 'px';
+    this.element.style.top = dimensions.top + 'px';
+    this.element.style.left = dimensions.left + 'px';
+  }
+
+  show(photo) {
+    // Display image with low-res image for fast loading
+    this.image = document.createElement('img');
+    this.image.style.backgroundImage = `url(/data/thumbs/${photo.id}.jpg)`;
+    this.element.appendChild(this.image);
+
+    this.overlay.style.display = 'block';
+    this.resize();
+    this.overlay.style.opacity = 1;
+
+    // Load high-res image
+    this.image.src = `/data/photos/${photo.id}.jpg`;
+  }
+
+  hide() {
+    this.overlay.style.opacity = 0;
+    this.overlay.style.display = 'none';
+    this.element.innerHTML = '';
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   const page = document.querySelector('#page');
   if (page) {
     window.page = new Page(page);
+  }
+  const lightbox = document.querySelector('#lightbox');
+  if (lightbox) {
+    window.lightbox = new Lightbox(lightbox);
   }
 });
 
