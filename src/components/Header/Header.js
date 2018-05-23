6accb30f985e238d27f89e70c619233a0d6d291e
Clement Bourgoin <cb@nokto.net>
2018-05-23 13:44:48 +0200

    Move Header component in its own file

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Header.scss';
+
+export default () => (
+  <div className="Header">
+    <h1 className="title">Road Trip
+      <br /> en Islande</h1>
+    <p className="subtitle">Mathilde &amp; Clément
+      <br /> sur les traces des macareux</p>
+  </div>
+);
