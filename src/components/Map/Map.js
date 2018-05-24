2d3e88baa953aaf43287102c151e3a1c001253e6
Clement Bourgoin <cb@nokto.net>
2018-05-24 12:47:40 +0200

    Add Markers to the map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
-import { getZoomForWidth } from './../../utils';
+import { getZoomForWidth } from '../../utils';
 
-const Map = (props) => (
-  <GoogleMap
-    defaultZoom={getZoomForWidth()}
-    defaultCenter={{ lat: 64.9313, lng: -19.0212 }}
-  >
-  </GoogleMap>
-);
+import entries from '../../data/entries';
+
+const Map = (props) => {
+
+  const markers = entries.map(({ id, coords, title }) => {
+    return <Marker key={id}
+      label={id}
+      position={{ lat: coords[0], lng: coords[1] }}
+      title={title}
+    />;
+  });
+
+  return (
+    <GoogleMap
+      defaultZoom={getZoomForWidth()}
+      defaultCenter={{ lat: 64.9313, lng: -19.0212 }}
+    >
+      {markers}
+    </GoogleMap>
+  )
+};
 
 export default withScriptjs(withGoogleMap(Map));
