af540717aee0acb4808c8baf25d1e05e82b32b0d
Clement Bourgoin <cb@nokto.net>
2018-05-24 12:31:07 +0200

    Display map at correct zoom level

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
+import { getZoomForWidth } from './../../utils';
+
 const Map = (props) => (
   <GoogleMap
-    defaultZoom={7}
+    defaultZoom={getZoomForWidth()}
     defaultCenter={{ lat: 64.9313, lng: -19.0212 }}
   >
   </GoogleMap>
