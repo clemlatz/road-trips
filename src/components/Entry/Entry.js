f3699846980ec90d514867b07e60e728c7bbe66a
Clement Bourgoin <cb@nokto.net>
2018-05-29 14:12:14 +0200

    Add links to open and close entry pages

--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Entry.scss';
 
@@ -8,13 +9,14 @@ export default (props) => {
 
   const id = props.match.params.id;
   const entry = entries.find(entry => entry.id === id);
-  console.log(entry);
 
   return (
     <article className="Entry">
       <span className="Entry-date">{entry.date}</span>
       <span className="Entry-id">{entry.id}</span>
-      <span className="Entry-close-button">×</span>
+      <Link to="/">
+        <span className="Entry-close-button">×</span>
+      </Link>
       <h1 className="Entry-title">{entry.title}</h1>
     </article>
   )
