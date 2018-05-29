66e7bb2bc3f2ced22cb8c20151e7659150d999b6
Clement Bourgoin <cb@nokto.net>
2018-05-29 13:39:59 +0200

    Add and configure react-router

--- src/app.js
+++ src/app.js
@@ -7,20 +7,27 @@ import 'whatwg-fetch';
 
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Entry from './components/Entry/Entry';
 import Header from './components/Header/Header';
 import Map from './components/Map/Map';
 
 const App = () => (
-  <Fragment>
-    <Header />
-    <Map
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJik81y-N4dkBXFH9p5sW99blqkXXwLP8"
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: '100vh', position: 'absolute', top: 0, width: '100vw' }} />}
-      mapElement={<div style={{ height: `100vh` }} />}
-    />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Header />
+      <Map
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJik81y-N4dkBXFH9p5sW99blqkXXwLP8"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: '100vh', position: 'absolute', top: 0, width: '100vw' }} />}
+        mapElement={<div style={{ height: `100vh` }} />}
+      />
+      <Switch>
+        <Route path="/entry/:id" component={Entry} />
+      </Switch>
+    </Fragment>
+  </Router>
 )
 
 ReactDOM.render(<App />,
