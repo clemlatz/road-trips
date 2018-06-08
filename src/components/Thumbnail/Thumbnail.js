6d398539d2aed68e8f7b9136b34028e392909e9b
Clement Bourgoin <cb@nokto.net>
2018-06-08 10:57:28 +0200

    Renamed Photo component to Thumbnail

---
+++ src/components/Thumbnail/Thumbnail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Thumbnail.scss';
+
+export default function Thumbnail({ id, caption }) {
+  return (
+    <div className="Thumbnail">
+      <img className="Thumbnail-image" src={`/data/thumbs/${id}.jpg`} alt={caption} />
+      {caption && <p className="Thumbnail-caption">{caption}</p>}
+    </div>
+  );
+}
+
+Thumbnail.propTypes = {
+  caption: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
