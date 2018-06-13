bc65eef61ce3a49d10aed6e54752d24edfd46eaf
Clement Bourgoin <cb@nokto.net>
2018-06-13 13:37:10 +0200

    Add Pin to Entry title

--- src/components/Pin/Pin.js
+++ src/components/Pin/Pin.js
@@ -4,10 +4,19 @@ import PropTypes from 'prop-types';
 import './Pin.scss';
 
 export default function Pin({ onClick, children }) {
-  return <div onClick={onClick} className="Pin">{children}</div>;
+  const classes = ['Pin'];
+  if (onClick) {
+    classes.push('clickable');
+  }
+
+  return <div onClick={onClick} className={classes.join(' ')}>{children}</div>;
 }
+
+Pin.defaultProps = {
+  onClick: null
+};
 
 Pin.propTypes = {
   children: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func,
 };
