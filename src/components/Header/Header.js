5ceda59d352a407dc4d1a2c74c6077d5caad8375
Clement Bourgoin <cb@nokto.net>
2018-06-25 17:34:56 +0200

    Add link on Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import './Header.scss';
 
@@ -7,15 +8,19 @@ export default function Header({ selectedTrip }) {
 
   let title = 'Road Trips';
   let subtitle = 'Journal de voyage de Mathilde & Clément';
+  let link = '/'; 
   if (selectedTrip) {
     title = selectedTrip.title;
     subtitle = selectedTrip.subtitle;
+    link = `/${selectedTrip.id}/`;
   }
 
   return (
     <div className="Header">
-      <h1 className="title">{title}</h1>
-      <p className="subtitle">{subtitle}</p>
+      <Link to={link}>
+        <h1 className="title">{title}</h1>
+        <p className="subtitle">{subtitle}</p>
+      </Link>
     </div>
   );
 }
